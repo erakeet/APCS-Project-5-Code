de649f5b20879becaa5f6ec51833fa4e417aee36
erakeet <66582183+erakeet@users.noreply.github.com>
2024-01-23 13:01:04 -0800

    Initial change
    Added a space.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Main { 
     static final int BOARDX = 7;
     static final int BOARDY = 15;
     public static void main(String[] args) {
